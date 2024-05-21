0cfce8df82824504a12e4f07003d0b4e0d0ffdaa
Rangga <rangga.yudhistira@ui.ac.id>
2024-05-21 13:46:47 +0700

    Initial Commit

---
+++ DokiDokiRaceWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DokiDokiRaceWatch Watch App
+//
+//  Created by Rangga Yudhistira Brata on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DokiDokiRaceWatch Watch App/DokiDokiRaceWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DokiDokiRaceWatchApp.swift
+//  DokiDokiRaceWatch Watch App
+//
+//  Created by Rangga Yudhistira Brata on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DokiDokiRaceWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
